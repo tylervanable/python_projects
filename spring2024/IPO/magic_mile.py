1b0b60e1a891243da0dcae0657af3d2ca8773068
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-02-04 01:11:10 -0500

    Update magic_mile.py

--- spring2024/IPO/magic_mile.py
+++ spring2024/IPO/magic_mile.py
@@ -5,7 +5,7 @@ calculate marathon time in hours using Galloway's Magic Mile formula.
 Tyler
 1/29/24
 """
-# Variable annotations
+# Variable annotations.
 MARATHON_MULTIPLIER: float = 1.3
 MARATHON_IN_MILES: float = 26.2
 SECONDS_IN_HOUR: int = 3600
@@ -18,10 +18,10 @@ marathon_time_hours: float
 # Obtain the user's pace in seconds for running a mile. 
 mile_pace = float(input("How many seconds does it take you to run a mile? "))
 
-# Calculate marathon time in hours using Galloway
+# Calculate marathon time in hours using Galloway's formula.
 marathon_mile_pace = float(mile_pace * MARATHON_MULTIPLIER)
 marathon_time_seconds = float(marathon_mile_pace * MARATHON_IN_MILES)
 marathon_time_hours = float(marathon_time_seconds/SECONDS_IN_HOUR)
 
-# Print marathon time in hours
+# Print the marathon time in hours.
 print("It will take you {} hour(s) to run a marathon".format(marathon_time_hours))
