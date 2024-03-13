7263e353dba4b5ae2bb3ac262577b9b9e5720425
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-03-12 22:57:06 -0400

    Create search_query.py

---
+++ libraries/ytmusicapi/search_query.py
@@ -0,0 +1,23 @@
+"""
+Display a list of search results utilizing the ytmusicapi library.
+
+Tyler
+"""
+search_results: str
+search: str
+
+# Import and initialize Youtube Music API 
+from ytmusicapi import YTMusic
+ytmusic = YTMusic("oauth.json")
+
+# Obtain a search query from the user.
+search = input("Please input a query to search for: ")
+
+# Search for the query and display the results.
+search_results = ytmusic.search(query=search, filter="songs", limit=1, ignore_spelling=True) # will search for 5 songs with the title "Oasis Wonderwall"
+print("\n Category:", search_results[0]["category"], 
+      "\n Title:", search_results[0]["title"], 
+      "\n Album:", search_results[0]["album"]["name"],
+      "\n Artist:", search_results[0]["artists"][0]["name"], 
+      "\n Length:", search_results[0]["duration"])
+
