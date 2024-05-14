784fc99e0a2640cdc53fdc1d66baeec8bf84c402
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-13 23:22:41 -0400

    Add files via upload

---
+++ summer2024/review/tip_calc.py
@@ -0,0 +1,21 @@
+"""
+    Calculate how much each person should pay for a meal with friends.
+    Consider the cost of the tip and how many people will split the bill.
+
+    Tyler
+    5/13/24
+"""
+
+
+# Annotate the variables and obtain user inputs.
+bill_cost: float = float(input("What was the total bill? "))
+tip_percentage: float = float(input("How much tip would you like to give? 15, 20, 25? "))
+num_people: int = int(input("How many people to split the bill? "))
+cost_per_person: float
+
+# Calculate and round the split cost of the bill.
+cost_per_person = (bill_cost + (bill_cost*tip_percentage)) / num_people
+cost_per_person = round(cost_per_person, 2)
+
+# Display the split cost of the bill.
+print(f"Each person should pay: ${cost_per_person}.")
