91bfc9264808d460c80d6ccc393e1e62de658ffc
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-13 23:44:26 -0400

    Update tip_calc2.py

--- summer2024/review/tip_calc2.py
+++ summer2024/review/tip_calc2.py
@@ -15,9 +15,9 @@ bill_after_tax: float = float(bill_cost*tip_percentage)
 num_people: int = int(input("How many people to split the bill? "))
 cost_per_person: float
 
-# Calculate and round the split cost of the bill.
+# Calculate the split cost of the bill.
 cost_per_person = bill_after_tax / num_people
-cost_per_person = round(cost_per_person, 2)
+cost_per_person = float("{:.2f}".format(cost_per_person))
 
 # Display the split cost of the bill.
 print(f"Each person should pay: ${cost_per_person}.")
