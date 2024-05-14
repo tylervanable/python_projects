dc63cdd47ccea2948b38fe2689e5270db6d7576a
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-13 23:48:08 -0400

    Update tip_calc2.py

--- summer2024/review/tip_calc2.py
+++ summer2024/review/tip_calc2.py
@@ -2,6 +2,9 @@
     Calculate how much each person should pay for a meal with friends.
     Consider the cost of the tip and how many people will split the bill.
 
+    This version includes the .format with format specifier. This version
+    also includes different variables for tip and tip percentage.
+
     Tyler
     5/13/24
 """
