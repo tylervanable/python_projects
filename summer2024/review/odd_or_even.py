9d4da710cade75b4ac023e7b52e79e23f62869ec
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-17 20:55:12 -0400

    Update odd_or_even.py

--- summer2024/review/odd_or_even.py
+++ summer2024/review/odd_or_even.py
@@ -6,15 +6,11 @@
 """
 
 
-# Annotate the variables and obtain a number from the user.
+# Annotate the variable and obtain a number from the user.
 number: int = int(input("Enter a number: "))
-remainder: int
 
-# Calculate the remainder of the user's number when dividing by 2.
-remainder = number % 2
-
-# Display whether the user's number is an even or odd number.
-if remainder == 0:
+# Calculate and display whether the user's number is an even or odd number.
+if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
