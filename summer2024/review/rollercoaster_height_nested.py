343ef977d0dcf11cfa51170ae54919b317e8fbcc
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-17 21:01:33 -0400

    Add files via upload

---
+++ summer2024/review/rollercoaster_height_nested.py
@@ -0,0 +1,43 @@
+"""
+    Check if the user is tall enough to ride a rollercoaster.
+    Utilizes a nested loop and elif statements.
+
+    Tyler
+    5/14/24
+"""
+
+
+# Annotate the variables.
+height: int
+age: int
+
+# Display a message and obtain the height from the user.
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# Display a message to the user whether or not if they can ride the rollercoaster.
+if height >= 120:
+    print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# Insert a line break.
+print("\n")
+
+# Discuss comparison operators.
+print("Comparison operators include the following: ")
+print("Greater than: '>'")
+print("Less than: '<'")
+print("Greater than or equal to: '>='")
+print("Less than or equal to: '<='")
+print("Equal to: '=='")
+print("Not equal to: '!='")
+print("Note: Make sure to distinguish between '=' and '=='.")
+print("... '=' will assign a value while '==' checks if two values are True or False")
