106467e362396ded9b0549fff977ca5497b723d1
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-19 23:24:28 -0400

    Update random_module.py

--- summer2024/review/random_module.py
+++ summer2024/review/random_module.py
@@ -1,16 +1,15 @@
 """
-    Demonstrate the import library.
-    Demonstrate how to import a custom module.
-
+    Demonstrate the import library by generating:
+    a random whole number between 1 and 10 inclusive
+    a random float number between 0 inclusive and 1 exclusive
+    a random float number between 0 inclusive and 5 exclusive
+    
     Tyler
     5/19/24
 """
 
 # Import the random library.
 import random
-
-# Import a custom library called pi_module.
-import pi_module
 
 # Generate a random whole number between 1 and 10 inclusive.
 random_integer = random.randint(1, 10)
@@ -27,15 +26,5 @@ print("We can generate a floating point number between 0 inclusive and 5 exclusi
 print("...multiplying our 'random_float' variable by 5.")
 random_float *= 5
 print(random_float)
-
-# Insert a line space.
-print()
-
-# Display a variable called 'pi' from the custom 'pi_module'.
-print("The name of the custom module is 'pi_module'.")
-print("The name of the custom variable is 'pi'")
-print("If we import pi_module, Writing 'pi_module.pi' will print")
-print("the variable from our custom module:")
-print(pi_module.pi)
 
 
