820d1bb1e004ae43b24526731c26aea4864f701d
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-19 23:25:35 -0400

    Update and rename random_module.py to custom_module.py

---
+++ summer2024/review/custom_module_ex/custom_module.py
@@ -0,0 +1,18 @@
+"""
+    Demonstrate how to import a custom module.
+
+    Tyler
+    5/19/24
+"""
+
+# Import a custom library called pi_module.
+import pi_module
+
+# Display a variable called 'pi' from the custom 'pi_module'.
+print("The name of the custom module is 'pi_module'.")
+print("The name of the custom variable is 'pi'")
+print("If we import pi_module, writing 'pi_module.pi' will print")
+print("the variable from our custom module:")
+print(pi_module.pi)
+
+
