4948003e539d114ac230c2f3d4863b2141432b4e
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-24 23:23:31 -0400

    Add files via upload

---
+++ summer2024/lists/methods2.py
@@ -0,0 +1,30 @@
+"""
+    Remove the Wii U from the list of Nintendo consoles.
+    Also insert the Switch console to the list.
+    Then sort the Nintendo consoles by alphabetical order.
+    Finally make a copy of original list.
+
+    Tyler
+    5/24/24
+"""
+
+
+# Annotate the variable.
+nintendo_consoles: list[str]
+
+# Create a list of some Nintendo consoles by chronological release date.
+nintendo_consoles = ["NES", "SNES", "N64", "GameCube", "Wii", "Wii U"]
+
+# Display the list of Nintendo consoles before removing the Wii U.
+print("Here is a chronological list of Nintendo consoles prior to 2017:")
+print(nintendo_consoles)
+
+# Insert a line break.
+print()
+
+# Remove the Wii U from the list of Nintendo consoles.
+# Display the list of Nintendo consoles.
+print("Here is a list of Nintendo consoles, with the removal of the")
+print("least profitable Nintendo console.")
+nintendo_consoles.remove("Wii U")
+print(nintendo_consoles)
