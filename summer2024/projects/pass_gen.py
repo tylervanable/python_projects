cbffdaac1afc4d0572fbdc2d452f4bf9a01cf7cd
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-26 21:15:12 -0400

    Update pass_gen.py

--- summer2024/projects/pass_gen.py
+++ summer2024/projects/pass_gen.py
@@ -1,7 +1,7 @@
 """
-    Generate a password for the user. Password is semi-random,
-    because the password contains first all letters, then all numbers,
-    then all symbols.
+    Generate a password for the user. Utilizes .shuffle() from 
+    the random library multiple times to re-order the list of selected
+    password characters.
 
     Tyler
     5/26/24
@@ -22,7 +22,7 @@ password: str
 letters: list[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers: list[str] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols: list[str] = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*' '(', ')', '+', '_', '-', '+', '=', '{', '[', '}', ']', '|']
-selected_chars: list[str] = []
+password_chars: list[str] = []
 
 # Display a welcome message to the user.
 print("Welcome to the PyPassword Generator!")
@@ -35,26 +35,22 @@ num_numbers = int(input(f"How many numbers would you like?\n"))
 # Generate random integers to select random letters in the list.
 # Then store as a list.
 for letter in range(num_letters):
-    random_int = random.randint(0, len(letters) - 1)
-    selected_chars.append(letters[random_int])
+    password_chars.append(random.choice(letters))
 
 # Generate random integers to select random numbers in the list.
 # Then store as a list.
 for number in range(num_numbers):
-    random_int = random.randint(0, len(numbers) - 1)
-    selected_chars.append(numbers[random_int])
+    password_chars.append(random.choice(numbers))
 
 # Generate random integers to select random numbers in the list.
 # Then store as a list.
 for symbol in range(num_symbols):
-    random_int = random.randint(0, len(symbols) - 1)
-    selected_chars.append(symbols[random_int])
+    password_chars.append(random.choice(symbols))
 
 # Shuffle the order of the selected characters list.
 for num_times in range(0, 11):
-    random.shuffle(selected_chars)
+    random.shuffle(password_chars)
 
 # Join and display the password.
-password = "".join(selected_chars)
+password = "".join(password_chars)
 print(f"Your password is: {password}.")
-    
