676330aba50b3a9ce228d87f10709c8644e03ed7
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-28 22:55:43 -0400

    Create day_of_week.py

---
+++ libraries/datetime/day_of_week.py
@@ -0,0 +1,14 @@
+"""
+    Quick attempt (under 3 min.) at using datetime library
+    to find the day of the week.
+
+    Tyler
+    2/24/24
+"""
+
+
+import datetime
+
+date = datetime.date.today()
+
+print("Today's date is {}.".format(date))
