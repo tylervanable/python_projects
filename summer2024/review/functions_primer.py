546ade494f0fe4a090d8e5b74bbde0b3054d6dfd
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-29 14:58:33 -0400

    Update functions_primer.py

--- summer2024/review/functions_primer.py
+++ summer2024/review/functions_primer.py
@@ -1,5 +1,5 @@
 """
-    Discuss and demonstrate basic functions.
+    Discuss syntax of basic functions.
 
     Tyler
     5/28/24
@@ -20,3 +20,26 @@ print()
 print("To call a function the syntax looks like:")
 print("my_function()")
 print("Note: functions require parantheses in Python.")
+
+# Insert a line break.
+print()
+
+# Discuss indentation required for functions.
+print("All lines of code within the function must be indented.")
+print("For example...")
+print("def obtain_input:")
+print("    answer = input('What is the capital of Uganda?' )")
+print("    if answer == Kampala:")
+print("        print('You are correct!')")
+print("    else:")
+print("        print('Try again!')")
+print("Note: indentation is still required for if-elif-else structures, even within function indentation.")
+
+# Insert a line break.
+print()
+
+# Discuss the use of spaces versus tabs for indentation.
+print("In PEP 8, the recommendation is spaces for indentation.")
+print("PEP 8 recommends 4 spaces per indent.")
+print("Tabs are easier, but the recommendation is to use spaces for ensuring consistency.")
+
