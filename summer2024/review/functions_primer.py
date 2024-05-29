1b6f209eac1310e8d74b5efc8527894278a03b16
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-05-28 22:53:36 -0400

    Add files via upload

---
+++ summer2024/review/functions_primer.py
@@ -0,0 +1,22 @@
+"""
+    Discuss and demonstrate basic functions.
+
+    Tyler
+    5/28/24
+"""
+
+
+# Discuss function defintiion syntax.
+print("Function syntax looks like:")
+print("def my_function():")
+print("    Do this")
+print("    Then this")
+print("    Finally this")
+
+# Insert a line break.
+print()
+
+# Discuss function calls.
+print("To call a function the syntax looks like:")
+print("my_function()")
+print("Note: functions require parantheses in Python.")
