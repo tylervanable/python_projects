cf708a8ee07e7f245aaa68d0b26f045a3fe4c7dd
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-07 14:45:36 -0400

    Update greeting_func.py

--- summer2024/review/greeting_func.py
+++ summer2024/review/greeting_func.py
@@ -2,7 +2,7 @@
     Greet the user with a randomly chosen greeting. Demonstrates a function
     with an input (with a parameter, the name of the input being passed through
     "name", and an argument, the value of the input being passed through "name".)
-
+    
     Tyler
     6/6/24, rev. 6/7/24
 """
@@ -12,14 +12,14 @@
 import random
 
 # Define the greet() function.
-def greet(name: str):
+def greet(name: str, location: str):
     """Randomly choose a greeting and display it to the user."""
 
     # Annotate and define the local variables.
     greeting_list: list[str] = [
         "Hello", "How do you do?", "Lovely weather we're having",
-        "Sup", "How's it going", "Good morning!", "Howdy",
-        "Hey", "How's life", "Long time no see!"
+        "Sup", "How's it going", "Good morning", "Howdy",
+        "Hey", "How's life", "Long time no see",
         "Nice to meet you", "It's an honor to meet you",
         "Good to see you", "good afternoon", "good evening"
         
@@ -31,10 +31,21 @@ def greet(name: str):
 
     # Greet the user.
     print(f"{greeting}, {name}")
+    print(f"{name}, what is it like in {location}? ")
 
 
-# Obtain the user's name.
+# Obtain the user's name and location.
 name = input("What is your name? ")
+location = input("Where are you? ")
 
 # Call the greet() function.
-greet(name)
+greet(name, location)
+
+# Display a line break.
+print()
+
+# Discuss positional arguments.
+print("When a function is invoked, it has a positional argument.")
+print("This means the order in which the variables are called...")
+print("...will impact the order of the variable parameters.")
+
