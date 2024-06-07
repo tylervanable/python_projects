377947fbada6e7d80e133af4af05542db92b8c18
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-07 13:56:47 -0400

    Update greeting_func.py

--- summer2024/review/greeting_func.py
+++ summer2024/review/greeting_func.py
@@ -2,7 +2,7 @@
     Greet the user with a randomly chosen greeting.
 
     Tyler
-    6/6/24
+    6/6/24, rev. 6/7/24
 """
 
 
@@ -10,14 +10,14 @@
 import random
 
 # Define the greet() function.
-def greet():
+def greet(name: str):
     """Randomly choose a greeting and display it to the user."""
 
     # Annotate and define the local variables.
     greeting_list: list[str] = [
-        "Hello", "How do you do?", "Lovely weather we're having!",
-        "Sup", "How's it going?", "Good morning!", "Howdy!",
-        "Hey", "How's life?", "Long time no see!"
+        "Hello", "How do you do?", "Lovely weather we're having",
+        "Sup", "How's it going", "Good morning!", "Howdy",
+        "Hey", "How's life", "Long time no see!"
         "Nice to meet you", "It's an honor to meet you",
         "Good to see you", "good afternoon", "good evening"
         
@@ -28,8 +28,11 @@ def greet():
     greeting = random.choice(greeting_list)
 
     # Greet the user.
-    print(greeting)
+    print(f"{greeting}, {name}")
 
+
+# Obtain the user's name.
+name = input("What is your name? ")
 
 # Call the greet() function.
-greet()
+greet(name)
