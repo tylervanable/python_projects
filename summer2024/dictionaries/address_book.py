bee983b7a4c781b64f05fe9d0ea79786204d6e44
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-12 23:57:28 -0400

    Add files via upload

---
+++ summer2024/dictionaries/address_book.py
@@ -0,0 +1,47 @@
+"""
+    Program that allows users to enter and store the name, address, and
+    telephone number of a friend or relative.
+
+    Tyler
+    6/12/24
+"""
+
+
+# Define the main() function.
+def main() -> None:
+    """Obtain a name, address, and telephone number from the user.
+       Store them to variables and write to a text document."""
+
+    # Annotate the local variables.
+    name: str = sample_name
+    address: str = sample_address
+    phone_number: str = sample_number
+    book_list: list[dict]
+    address_book = {
+        "name": name
+        "address": address
+        "number": phone_number
+    }
+
+    # Obtain a text file name from the user.
+    text_file = input("Enter a text file name (without .txt): ")
+
+    # Obtain the user's choice for continuing the program.
+
+    # Read the user's past address book.
+
+    # If the user enters -1, cancel the program.
+
+    # If the user enters 0, obtain a name, address, and phone number
+    # from the user. Store to variables, a dictionary, and list.
+    # Write the dictionary to a text file.
+
+    # If the user enters 1, display the user's current address book.
+    # Allow users the opportunity to edit.
+
+    # If the user enters 2, display the user's current address book.
+
+    # If the user enters 3, delete everything in the text file and the program.
+
+    
+    
