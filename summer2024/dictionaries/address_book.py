4ae26e7449ab1d10c5fcb8e4448187fe1e908096
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-13 21:48:18 -0400

    Update address_book.py

--- summer2024/dictionaries/address_book.py
+++ summer2024/dictionaries/address_book.py
@@ -3,7 +3,7 @@
     telephone number of a friend or relative.
 
     Tyler
-    6/12/24
+    6/13/24
 """
 
 
@@ -13,6 +13,7 @@ def main() -> None:
        Store them to variables and write to a text document."""
 
     # Annotate the local variables.
+    text_file: str
     name: str = sample_name
     address: str = sample_address
     phone_number: str = sample_number
@@ -27,21 +28,43 @@ def main() -> None:
     text_file = input("Enter a text file name (without .txt): ")
 
     # Obtain the user's choice for continuing the program.
-
+    print("Type -1 to REMOVE an entry, type 0 to ADD an entry,")
+    print("type 1 to EDIT an entry, type 2 to VIEW all entries,")
+    print("or type 3 to ERASE all entries.")
+    user_choice = input("Enter -1, 0, 1, 2, or 3: ")
+    
     # Read the user's past address book.
-
+    ####TODO
+    
     # If the user enters -1, cancel the program.
+    while user_choice != -1:
+        
+        # If the user enters 0, obtain a name, address, and phone number
+        # from the user. Store to variables, a dictionary, and list.
+        # Write the dictionary to a text file.
+        if user_choice == 0:
+            name.title() = input("Enter the name of a new entry to your address book:\n")
+            address = input(f"Enter {name}'s address:\n") 
+            phone_number = input(f"Enter {name}'s phone number:\n")
+            address_book = add_to_dict()
+            book_list.append(address_book)
+            write_to_file()
 
-    # If the user enters 0, obtain a name, address, and phone number
-    # from the user. Store to variables, a dictionary, and list.
-    # Write the dictionary to a text file.
+        # If the user enters 1, display the user's current address book.
+        # Allow users the opportunity to edit.
+        if user_choice == 1:
+            a
+            
+        # If the user enters 2, display the user's current address book.
+        if user_choice == 2:
+            asd
+        # If the user enters 3, delete everything in the text file and the program.
+        if user_choice == 3:
+            
+        # Obtain the user's choice again.
+        print("Type -1 to REMOVE, 0 to ADD, 1 to EDIT, 2 to VIEW, or 3 to ERASE.")
+        user_choice = input("Enter -1, 0, 1, or 2: ")
 
-    # If the user enters 1, display the user's current address book.
-    # Allow users the opportunity to edit.
-
-    # If the user enters 2, display the user's current address book.
-
-    # If the user enters 3, delete everything in the text file and the program.
 
     
     
