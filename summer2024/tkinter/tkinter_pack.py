f9cd0400eb04c015329abfb0b672553a816c983b
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-20 11:24:38 -0400

    Add files via upload

---
+++ summer2024/tkinter/tkinter_pack.py
@@ -0,0 +1,28 @@
+"""
+    Display a window screen with a title to the user.
+
+    Tyler
+    6/19/24
+"""
+
+# Import the tkinter module.
+import tkinter
+
+# Initialize the window screen that displays to the user.
+window = tkinter.Tk()
+
+# Display a title to the user with a "minimum size" of 500x300 px.
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+# Display a label text to the user with Arial font, 24 size font, and bold.
+# and "pack" it to the left side of the screen.
+first_label = tkinter.Label(text="First Label!", font=("Arial", 24, "bold"))
+first_label.pack(side="left") # could also do "right"
+
+
+
+
+
+# Initalize the main loop to keep the window on the screen.
+window.mainloop() # always on very end of program
---
+++ summer2024/tkinter/tkinter_window.py
@@ -0,0 +1,21 @@
+"""
+    Display a window screen with a title to the user.
+
+    Tyler
+    6/19/24
+"""
+
+# Import the tkinter module.
+import tkinter
+
+# Initialize the window screen that displays to the user.
+window = tkinter.Tk()
+
+# Display a title to the user.
+window.title("My first GUI Program")
+
+
+
+
+# Initalize the main loop to keep the window on the screen.
+window.mainloop() # always on very end of program
