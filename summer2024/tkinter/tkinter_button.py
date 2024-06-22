4906252fb6d2fc7785c8941e616b58f0eb5c34a4
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-22 18:10:13 -0400

    Add files via upload

---
+++ summer2024/tkinter/tkinter_button.py
@@ -0,0 +1,50 @@
+"""
+
+"""
+
+
+# Import the tkinter module.
+import tkinter
+
+# Display the 500x300 window with a title
+window = tkinter.Tk()
+window.title("My Next GUI Program")
+window.minsize(width=500, height=300)
+
+# Display a label.
+my_label = tkinter.Label(text="I am Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+# Change the label text.
+my_label["text"] = "This is the new text..."
+
+# Change the label text a different way.
+my_label.config(text="This is the NEW, new text...")
+
+# Define a button_clicked function.
+def button_clicked() -> None:
+    """Display a message to the user when the button is clicked."""
+
+    # Display the message, change the title.
+    print("I got clicked...")
+    window.title("Button clicked...")
+    my_label.config(text="CLICKETH THE BUTTONETH")
+
+    # Store the user input and display it.
+    new_text = input.get()
+    my_label.config(text=new_text.upper())
+    
+# Create a button.
+button = tkinter.Button(text="Click me please", command=button_clicked)
+button2 = tkinter.Button(text="Store the text", command=button_clicked)
+button.pack()
+button2.pack()
+
+# Display a typable Entry widget to the user.
+input = tkinter.Entry(width=40)
+input.pack()
+
+# Store the user input as a variable.
+
+# Execute main loop
+tkinter.mainloop()
