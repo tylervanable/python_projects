4ede7f4029254256c6703a93680cff35ce0e70ea
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-22 23:04:53 -0400

    Update dog_food_schedule.py

--- spring2024/branching/dog_food_schedule.py
+++ spring2024/branching/dog_food_schedule.py
@@ -1,6 +1,7 @@
 """
-    Ask user if it is morning, afternoon, or evening, and remind the user
-    to feed their pet if it is morning
+    Obtain the current hour from the user in military time.
+    Determine if it is morning, afternoon, or evening, 
+    and remind the user to feed their pet if it is morning.
 
     Tyler
     2/24/24
@@ -14,7 +15,7 @@ hour: int
 # Obtain the hour number from the user.
 hour = int(input("Please input the current hour as a whole number in military time: "))
 
-# Display whether it is morning, afternoon, or evening.
+# Display a message depending of it is morning, afternoon, or evening.
 if hour >= 1 and hour < 12:
     print("It's morning, so don't forget to feed your dog!")
 elif hour >= 12 and hour < 17:
