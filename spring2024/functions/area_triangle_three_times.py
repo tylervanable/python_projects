805f3032f45df655d465ad9f1e98bb1e45e54bd4
tylervanable <152121682+tylervanable@users.noreply.github.com>
2024-06-22 23:02:43 -0400

    Update area_triangle_three_times.py

--- spring2024/functions/area_triangle_three_times.py
+++ spring2024/functions/area_triangle_three_times.py
@@ -1,14 +1,19 @@
 """
-    Calculate the area of a triangle for the user.
+    Calculate the area of a triangle for the user three times.
     Write a function that takes a triangle’s base and height as (parameter) input and returns the area.
-    
+
+    Function calculate_area will calculate the area of a triangle and return to main.
+    Function main is where execution begins. Inputs are obtained from the user and the
+    calculate_area function is called three times. Three different areas are displayed
+    to the user.
+
     Tyler
     4/3/24
 """
 
 
 def calculate_area(base: float, height: float) -> float:
-    """Calculate the area of three values from the user."""
+    """Calculate the area of a triangle from the user."""
 
     # Annotate the variable.
     area: float
@@ -21,14 +26,14 @@ def calculate_area(base: float, height: float) -> float:
 
 
 def main() -> None:
-    """Obtain the base and height from the user and invoke the calculate_area function."""
+    """Obtain the base and height from the user and invoke the calculate_area function three times."""
 
     # Annotate the variables.
     base: float
     height: float
 
     # Obtain the base and height from the user three times.
-    for i in range(1, 4):
+    for i in range(0, 3):
         base = float(input("Please input the base of the triangle: "))
         height = float(input("Please input the height of the triangle: "))
 
